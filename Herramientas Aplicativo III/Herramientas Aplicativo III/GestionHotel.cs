0060e047447f8f4f893c40213d25534054035e45
Sanchez988 <valentina.roldan339@pascualbravo.edu.co>
2025-03-11 22:28:39 -0500

    Add files via upload

---
+++ Herramientas Aplicativo III/Herramientas Aplicativo III/GestionHotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herramientas_Aplicativo_III.Entidades
+
+{
+        public class Reserva
+        {
+            public int Id_Reserva { get; set; }
+            public int Id_Cliente { get; set; }
+            public int Id_Habitacion { get; set; }
+            public DateTime Fecha_Inicio { get; set; }
+            public DateTime Fecha_Fin { get; set; }
+            public string Estado { get; set; }
+            public decimal Monto_Total { get; set; }
+            public string Metodo_Pago { get; set; }
+        }
+    }
+
+
---
+++ Herramientas Aplicativo III/Herramientas Aplicativo III/ReservaBLL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Herramientas_Aplicativo_III.Entidades;
+using Herramientas_Aplicativo_III.Datos;
+
+namespace Herramientas_Aplicativo_III.Logica
+{
+        public class ReservaBLL
+        {
+            private ReservaDAL reservaDAL = new ReservaDAL();
+
+            public void CrearReserva(Reserva reserva)
+            {
+                reservaDAL.CrearReserva(reserva);
+            }
+
+            public Reserva ConsultarReserva(int idReserva)
+            {
+                return reservaDAL.ConsultarReserva(idReserva);
+            }
+        }
+    }
